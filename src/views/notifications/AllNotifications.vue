a5e03840599fc9db31aa2fbe61de9fb732d7f824
Kasra Karami <kasra.karami.kk@gmail.com>
2021-05-04 03:15:19 +0430

    fix divider

--- src/views/notifications/AllNotifications.vue
+++ src/views/notifications/AllNotifications.vue
@@ -89,7 +89,7 @@
               <v-toolbar-title>
                 <div class="d-flex justify-start align-center">
                   <h1 class="blue--text">{{ title }}</h1>
-                  <v-divider inset vertical class="mx-4"></v-divider>
+                  <v-divider vertical class="mx-4"></v-divider>
                   <router-link :to="{ name: 'CreateNotification' }">
                     <v-icon color="blue" large>mdi-plus-circle</v-icon>
                   </router-link>
