dec0ecb37ed31f8c5e2c4660a8bbdb8f9fc7f463
Kasra Karami <kasra.karami.kk@gmail.com>
2021-05-24 17:52:10 +0430

    make vuetify clean nad default is now dark

---
+++ src/plugins/theme.ts
@@ -0,0 +1,42 @@
+import colors from "vuetify/lib/util/colors";
+
+export const light = {
+  // class
+  primary: "#003974",
+  secondary: "#A7A8A9",
+  warning: "#FEC233",
+  error: "#F51F1F",
+  success: "#32BE93",
+  info: "#2196F3",
+  accent: "#82B1FF",
+  // link
+  anchor: "#003974",
+  // colors
+  blue: "#003974",
+  grey: "#A7A8A9",
+  gray: "#D0D0CE",
+  yellow: "#FEC233",
+  red: "#F51F1F",
+  green: "#32BE93",
+};
+
+export const dark = {
+  // class
+  primary: "#0066d0",
+  secondary: "#A7A8A9",
+  warning: "#FEC233",
+  error: "#F51F1F",
+  success: "#32BE93",
+  info: "#2196F3",
+  accent: "#82B1FF",
+  // link
+  anchor: "#82B1FF",
+  // colors
+  blue: "#0066d0",
+  grey: "#ACACAC",
+  gray: "#D0D0CE",
+  yellow: "#FEC233",
+  red: "#F51F1F",
+  green: "#32BE93",
+  background: "#1A1A2F",
+};
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 import { fa } from "vuetify/src/locale";
+import { light, dark } from "@/plugins/theme";
 
 Vue.use(Vuetify);
 
@@ -21,49 +22,9 @@ export default new Vuetify({
     },
   },
   theme: {
-    dark: false,
-    default: "light",
+    dark: true,
+    default: "dark",
     disable: false,
-    themes: {
-      light: {
-        // class
-        primary: "#003974",
-        secondary: "#A7A8A9",
-        warning: "#FEC233",
-        error: "#F51F1F",
-        success: "#32BE93",
-        info: "#2196F3",
-        accent: "#82B1FF",
-        // link
-        anchor: "#003974",
-        // colors
-        blue: "#003974",
-        grey: "#A7A8A9",
-        gray: "#D0D0CE",
-        yellow: "#FEC233",
-        red: "#F51F1F",
-        green: "#32BE93",
-      },
-      dark: {
-        // class
-        primary: "#0066d0",
-        secondary: "#A7A8A9",
-        warning: "#FEC233",
-        error: "#F51F1F",
-        success: "#32BE93",
-        info: "#2196F3",
-        accent: "#82B1FF",
-        // link
-        anchor: "#82B1FF",
-        // colors
-        blue: "#0066d0",
-        grey: "#ACACAC",
-        gray: "#D0D0CE",
-        yellow: "#FEC233",
-        red: "#F51F1F",
-        green: "#32BE93",
-        background: "#1A1A2F",
-      },
-    },
+    themes: { light, dark },
   },
 });
