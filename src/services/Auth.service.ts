bcb4582dd77696c3149d01970ecb62378e8e9f2b
Kasra Karami <kasra.karami.kk@gmail.com>
2021-07-12 11:57:53 +0430

    change login from customer to admin

--- src/services/Auth.service.ts
+++ src/services/Auth.service.ts
@@ -6,13 +6,13 @@ export default {
    * @param {*} data: { phoneNumber: string }
    */
   async getPin(data: { phoneNumber: string }): Promise<AxiosResponse> {
-    return await apiClient.post("/otp/request", data);
+    return await apiClient.post("/otp/request/admin", data);
   },
 
   async login(data: {
     phoneNumber: string;
     pin: string;
   }): Promise<AxiosResponse> {
-    return await apiClient.post("/customerAuth", data);
+    return await apiClient.post("/adminAuth", data);
   },
 };
