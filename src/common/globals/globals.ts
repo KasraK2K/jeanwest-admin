b801fd289c1e978b2fcf2dea63ad079ee44fb6e3
Kasra_K2K <Kasra_K2K@yahoo.com>
2021-08-31 17:56:04 +0430

    remove all production config

--- src/common/globals/globals.ts
+++ src/common/globals/globals.ts
@@ -11,10 +11,12 @@ export const tokenName = "token";
 
 export const globals = {
   /** NOTE: Server Backend */
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "http://10.0.1.131:5000/api/v1"
-      : "http://10.0.1.111:8000/api/v1",
+  // baseURL:
+  // process.env.NODE_ENV === "production"
+  //   ? "http://10.0.1.131:5000/api/v1"
+  //   : "http://10.0.1.111:8000/api/v1",
+
+  baseURL: "http://10.0.1.111:8000/api/v1",
 
   /** NOTE: Server Backend Local */
   // baseURL: "http://localhost:3500/api/v1",
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  lintOnSave: process.env.NODE_ENV !== "production",
+  // lintOnSave: process.env.NODE_ENV !== "production",
   configureWebpack: {
     devtool: "source-map",
   },
-  devServer: {
-    host: process.env.HOST || "localhost",
-    port: process.env.PORT || "8080",
-    https: JSON.parse(process.env.HTTPS) || false,
-    overlay: {
-      warnings: false,
-      errors: process.env.NODE_ENV !== "production",
-    },
-  },
+  // devServer: {
+  //   host: process.env.HOST || "localhost",
+  //   port: process.env.PORT || "8080",
+  //   https: JSON.parse(process.env.HTTPS) || false,
+  //   overlay: {
+  //     warnings: false,
+  //     errors: process.env.NODE_ENV !== "production",
+  //   },
+  // },
 };
