1ebc89bb90ee0202eadc73e369434526e56e61fc
Kasra_K2K <Kasra_K2K@yahoo.com>
2021-09-21 13:48:32 +0430

    remove unnecessary config

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,16 @@
-const production = process.env.NODE_ENV === "production";
-
 module.exports = {
   transpileDependencies: ["vuetify"],
-  lintOnSave: !production,
+  lintOnSave: process.env.NODE_ENV !== "production",
   configureWebpack: {
     devtool: "source-map",
   },
   devServer: {
-    host: production ? process.env.HOST || "0.0.0.0" : "localhost",
-    port: production ? process.env.PORT || 8080 : 8080,
+    host: process.env.HOST,
+    port: process.env.PORT,
     https: JSON.parse(process.env.HTTPS),
     overlay: {
       warnings: false,
-      errors: !production,
+      errors: process.env.NODE_ENV !== "production",
     },
     public: "https://admin.avktn.com",
     // disableHostCheck: true,
