2f366127661cebf54f2e4d1fdb1f4502108663c4
Kasra Karami <kasra_k2k@yahoo.com>
2021-10-18 14:15:11 +0330

    add mock page for edit admin

--- src/router/compact/admins.routes.ts
+++ src/router/compact/admins.routes.ts
@@ -1,5 +1,6 @@
 import { IRouter } from "@/interfaces/others/route.interface";
 import AllAdmins from "@/views/admins/list/AllAdmins.vue";
+import EditAdmin from "@/views/admins/edit/EditAdmin.vue";
 
 export const admins: IRouter[] = [
   {
@@ -8,6 +9,15 @@ export const admins: IRouter[] = [
     component: AllAdmins,
     meta: {
       title: "لیست ادمین‌ها",
+    },
+  },
+  {
+    path: "/admins/edit/:id",
+    name: "EditAdmin",
+    component: EditAdmin,
+    props: true,
+    meta: {
+      title: "ویرایش ادمین‌ها",
     },
   },
 ];
---
+++ src/views/admins/edit/EditAdmin.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container fluid>
+    <h1 class="blue--text">{{ title }}</h1>
+    <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
+
+    <v-row>
+      <v-col cols="12">
+        <v-card elevation="2" outlined id="jeanspoint" class="mt-2 pa-4">
+          <v-form @submit.prevent="updateAdmin">
+            <v-btn type="submit" large color="primary" class="mt-4"
+              >ویرایش</v-btn
+            >
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    const title = "ویرایش ادمین ";
+    return {
+      title,
+      breadcrumbs: [
+        {
+          text: "صفحه اصلی",
+          to: "/",
+        },
+        {
+          text: "لیست ادمین‌ها",
+          to: { name: "AllAdmins" },
+        },
+        {
+          text: title,
+          to: document.location.pathname,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    updateAdmin() {
+      alert("oops");
+    },
+  },
+});
+</script>
